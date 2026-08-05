a130dc1ba3e1bed9422c7fa0fb066d21dbdccdc2
Jashnoor Singh <jashnoorsingh2005@gmail.com>
2026-08-05 14:30:52 +0530

    improving

--- Views/Home/DaysView.swift
+++ Views/Home/DaysView.swift
@@ -14,19 +14,24 @@ struct DaysView: View{
         "Sun","Mon","Tue",
         "Wed","Thu","Fri","Sat" ]
     var body: some View{
-        HStack{
-            ForEach(days.indices, id: \.self){ index in
-                if let date = Calendar.current.date(
-                    byAdding: .day,
-                    value: index + 1 - weekday,
-                    to: Date()
-                ){
-                    let dayNumber = Calendar.current.component(.day, from: date)
-                    daysRect(
-                        day: days[index],
-                        variable: weekday == index + 1,
-                        currDate: dayNumber
-                    )
+        VStack{
+            RoundedRectangle(cornerRadius: 40.45)
+                .frame(height: 183.25)
+                
+            HStack{
+                ForEach(days.indices, id: \.self){ index in
+                    if let date = Calendar.current.date(
+                        byAdding: .day,
+                        value: index + 1 - weekday,
+                        to: Date()
+                    ){
+                        let dayNumber = Calendar.current.component(.day, from: date)
+                        daysRect(
+                            day: days[index],
+                            variable: weekday == index + 1,
+                            currDate: dayNumber
+                        )
+                    }
                 }
             }
         }.padding()
